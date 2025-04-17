424e15fdfcddeb92a29d5b1dfaa21e7a66ac9106
matthew-pisano <matthewpisano14@gmail.com>
2025-04-16 22:29:49 -0400

    Add Tests for String to Byte Conversions

--- test/fixtures/hello_world.asm
+++ test/fixtures/hello_world.asm
@@ -4,10 +4,10 @@
 
 out_string: .asciiz "\nHello, World!"
 out_string2: .asciiz "\nHello, World!"
-label:
+label: .word 42
 .text ## Assembly language instructions go in text segment
 main: ## Start of code section
-    addiu $v0, $zero, label # system call code for printing string = 4
+    addiu $v0, $zero, 4 # system call code for printing string = 4
     la $a0, out_string # load address of string to be printed into $a0
     syscall # call operating system to perform operation
     # specified in $v0
--- test/test_parser.cpp
+++ test/test_parser.cpp
@@ -1,3 +1,61 @@
 //
 // Created by matthew on 4/16/25.
 //
+
+
+#include <catch2/catch_test_macros.hpp>
+
+#include "parser.h"
+
+
+TEST_CASE("Test String to Bytes") {
+    SECTION("Test Single Char") {
+        std::vector<uint8_t> expectedBytes = {0x61};
+        std::vector<uint8_t> actualBytes = stringToBytes("a");
+        REQUIRE(expectedBytes == actualBytes);
+    }
+    SECTION("Test Multiple Char") {
+        std::vector<uint8_t> expectedBytes = {0x61, 0x20, 0x62, 0x47, 0x2f, 0x3f, 0x2e, 0x31};
+        std::vector<uint8_t> actualBytes = stringToBytes("a bG/?.1");
+        REQUIRE(expectedBytes == actualBytes);
+    }
+}
+
+
+TEST_CASE("Test Integer String to Bytes") {
+    SECTION("Test Positive") {
+        std::vector<uint8_t> expectedBytes = {0x00, 0x00, 0x00, 0x00};
+        std::vector<uint8_t> actualBytes = intStringToBytes("0");
+        REQUIRE(expectedBytes == actualBytes);
+
+        expectedBytes = {0x00, 0x00, 0x00, 0x01};
+        actualBytes = intStringToBytes("1");
+        REQUIRE(expectedBytes == actualBytes);
+
+        expectedBytes = {0x00, 0x05, 0x7c, 0x3a};
+        actualBytes = intStringToBytes("359482");
+        REQUIRE(expectedBytes == actualBytes);
+
+        expectedBytes = {0x7f, 0xff, 0xff, 0xff};
+        actualBytes = intStringToBytes("2147483647");
+        REQUIRE(expectedBytes == actualBytes);
+
+        REQUIRE_THROWS(intStringToBytes("2147483648"));
+    }
+
+    SECTION("Test Negative") {
+        std::vector<uint8_t> expectedBytes = {0xff, 0xff, 0xff, 0xff};
+        std::vector<uint8_t> actualBytes = intStringToBytes("-1");
+        REQUIRE(expectedBytes == actualBytes);
+
+        expectedBytes = {0xff, 0xfa, 0x83, 0xc6};
+        actualBytes = intStringToBytes("-359482");
+        REQUIRE(expectedBytes == actualBytes);
+
+        expectedBytes = {0x80, 0x00, 0x00, 0x00};
+        actualBytes = intStringToBytes("-2147483648");
+        REQUIRE(expectedBytes == actualBytes);
+
+        REQUIRE_THROWS(intStringToBytes("-2147483649"));
+    }
+}
