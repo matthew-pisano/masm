7e46b068953a09410861dc15d8a1b6d0ffaa7a91
matthew-pisano <matthewpisano14@gmail.com>
2025-04-20 00:21:18 -0400

    Add Memory Helper Documentation

--- include/memory.h
+++ include/memory.h
@@ -8,13 +8,25 @@
 #include <string>
 
 
+/**
+ * Class representing valid, named sections of memory
+ */
 enum class MemSection { DATA, TEXT };
 
 
+/**
+ * Returns the memory section associated with a mnemonic
+ * @param name The name of the memory section
+ * @return The associated memory section
+ */
 MemSection nameToMemSection(const std::string& name);
 
 
+/**
+ * Returns the static offset of a named memory section
+ * @param section The section of memory
+ * @return The memory offset in bytes
+ */
 uint32_t memSectionOffset(MemSection section);
-
 
 #endif // MEMORY_H
