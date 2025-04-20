6adce8fa13ee1bbbb3139ed10afec365dd15825f
matthew-pisano <matthewpisano14@gmail.com>
2025-04-20 00:58:13 -0400

    Add Documentation for Utils

--- include/utils.h
+++ include/utils.h
@@ -9,14 +9,46 @@
 #include <tokenizer.h>
 #include <vector>
 
+
+/**
+ * Checks if a string is a value signed integer
+ * @param str The string to check
+ * @return True if the string is a signed integer, false otherwise
+ */
 bool isSignedInteger(const std::string& str);
 
+
+/**
+ * Converts a string to a vector of bytes, where each byte is the ASCII value of the character
+ * @param string The string to convert
+ * @return The vector of bytes representing the string
+ */
 std::vector<uint8_t> stringToBytes(const std::string& string);
 
+
+/**
+ * Converts a string representing an integer into a 32-bit big endian representation in bytes
+ * @param string The string to convert
+ * @return The vector of bytes representing the integer
+ */
 std::vector<uint8_t> intStringToBytes(const std::string& string);
 
+
+/**
+ * Validates a comma seperated list of tokens, returning the list with commas stripped out
+ * @param listTokens The list of tokens to filter
+ * @return The filtered list of tokens
+ * @throw runtime_error When the list is malformed
+ */
 std::vector<Token> filterTokenList(const std::vector<Token>& listTokens);
 
+
+/**
+ * Checks to see if a given vector of tokens matches a token type pattern
+ * @param pattern The pattern to match against
+ * @param tokens The tokens to check
+ * @return True if the tokens match the pattern, false otherwise
+ */
 bool tokenTypeMatch(const std::vector<TokenType>& pattern, const std::vector<Token>& tokens);
 
 #endif // UTILS_H
