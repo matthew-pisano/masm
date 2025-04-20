d5a9bb8ae26c0fccbf588dfcf9051c977913074d
matthew-pisano <matthewpisano14@gmail.com>
2025-04-20 00:19:04 -0400

    Add Documentation for Instruction Functions

--- include/instruction.h
+++ include/instruction.h
@@ -7,6 +7,10 @@
 #include <cstdint>
 #include "tokenizer.h"
 
+
+/**
+ * Class representing all valid instruction types and sub-types
+ */
 enum class InstructionType {
     R_TYPE,
     I_TYPE,
@@ -17,6 +21,10 @@ enum class InstructionType {
     PSEUDO
 };
 
+
+/**
+ * Class Detailing the details of a specific instruction
+ */
 struct InstructionOp {
     InstructionType type;
     uint8_t opFuncCode;
@@ -24,10 +32,29 @@ struct InstructionOp {
 };
 
 
+/**
+ * Fetches the instruction associated with its name
+ * @param name The name of the instruction
+ * @return The instruction representation
+ */
 InstructionOp nameToInstructionOp(const std::string& name);
 
+
+/**
+ * Ensures that the arguments of an instruction match an expected pattern of token types
+ * @param instruction The instruction token
+ * @param args A list of arguments following the given instruction
+ * @throw runtime_error when the arguments for an instruction do not match its accepted values
+ */
 void validateInstruction(const Token& instruction, const std::vector<Token>& args);
 
+
+/**
+ * Ensures that the arguments of a pseudo instruction match an expected pattern of token types
+ * @param instruction The pseudo instruction token
+ * @param args A list of arguments following the given pseudo instruction
+ * @throw runtime_error when the arguments for a pseudo instruction do not match its accepted values
+ */
 void validatePseudoInstruction(const Token& instruction, const std::vector<Token>& args);
 
 #endif // INSTRUCTION_H
