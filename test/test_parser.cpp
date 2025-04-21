73453edd0284ed4346e5675b8f614798616c9114
matthew-pisano <matthewpisano14@gmail.com>
2025-04-21 19:16:54 -0400

    Add Validate Memory Layout Function to Parser Tests

--- test/test_parser.cpp
+++ test/test_parser.cpp
@@ -5,7 +5,48 @@
 
 #include <catch2/catch_test_macros.hpp>
 
+#include "fileio.h"
 #include "parser.h"
+
+
+/**
+ * Validates the memory layout of a parsed file against the loaded, expected memory
+ * @param sourceFileName The name of the source file to parse
+ * @param parsedFileName The name of the parsed file to compare against
+ */
+void validateMemLayout(const std::string& sourceFileName, const std::string& parsedFileName) {
+    const std::vector<unsigned char> parsedBytes = readFileBytes(parsedFileName);
+
+    MemLayout expectedMem;
+    MemSection currSection;
+    for (size_t i = 0; i < parsedBytes.size(); ++i) {
+        constexpr unsigned char groupSep = 0x1d;
+        if (i < parsedBytes.size() - 1 && parsedBytes[i] == groupSep &&
+            (parsedBytes[i + 1] == 0 || parsedBytes[i + 1] == 1)) {
+            currSection = static_cast<MemSection>(parsedBytes[i + 1]);
+            i += 1;
+            continue;
+        }
+        expectedMem[currSection].push_back(parsedBytes[i]);
+    }
+
+    const std::vector<std::string> sourceLines = readFileLines(sourceFileName);
+    const std::vector<std::vector<Token>> program = Tokenizer::tokenize(sourceLines);
+    Parser parser{};
+    MemLayout actualMem = parser.parse(program);
+    REQUIRE(expectedMem.size() == actualMem.size());
+    for (const std::pair<const MemSection, std::vector<unsigned char>>& pair : expectedMem) {
+        REQUIRE(actualMem.contains(pair.first));
+
+        REQUIRE(pair.second == actualMem[pair.first]);
+    }
+}
+
+
+TEST_CASE("Test Hello World") {
+    validateMemLayout("test/fixtures/hello_world/hello_world.asm",
+                      "test/fixtures/hello_world/hello_world.pse");
+}
 
 
 TEST_CASE("Test Parse Labels") {
