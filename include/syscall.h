6785ccc3311b2c3e36bf9a49d29f9d5907bfa97e
matthew-pisano <matthewpisano14@gmail.com>
2025-04-26 11:17:51 -0400

    Add Syscall File

---
+++ include/syscall.h
@@ -0,0 +1,8 @@
+//
+// Created by matthew on 4/26/25.
+//
+
+#ifndef SYSCALL_H
+#define SYSCALL_H
+
+#endif //SYSCALL_H
---
+++ src/syscall.cpp
@@ -0,0 +1,5 @@
+//
+// Created by matthew on 4/26/25.
+//
+
+#include "syscall.h"
