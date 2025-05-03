02a5e028a37641dbbd68880d7293627098308c76
matthew-pisano <matthewpisano14@gmail.com>
2025-05-03 00:27:49 -0400

    Use Allocation Logic for existing Directives and add Align

--- src/directive.cpp
+++ src/directive.cpp
@@ -9,7 +9,7 @@
 #include "utils.h"
 
 
-std::vector<std::byte> parseAllocDirective(uint32_t loc, const Token& dirToken,
+std::vector<std::byte> parseAllocDirective(const uint32_t loc, const Token& dirToken,
                                            const std::vector<Token>& args) {
     const std::string dirName = dirToken.value;
     if (args.empty())
@@ -17,44 +17,61 @@ std::vector<std::byte> parseAllocDirective(uint32_t loc, const Token& dirToken,
 
     std::vector<std::byte> bytes = {};
 
-    // Return a byte vector containing each character of the string
+    // Insert a string
     if (dirName == "asciiz" || dirName == "ascii") {
         if (args.size() != 1)
             throw std::runtime_error(dirName + " directive expects exactly one argument");
         if (args[0].type != TokenType::STRING)
             throw std::runtime_error(dirName + " directive expects a string argument");
-        std::string escapedString = escapeString(args[0].value);
-        return stringToBytes(escapedString, dirName == "asciiz");
-    }
-    // Return a word for each argument given
-    if (dirName == "word") {
-        for (const Token& arg : args) {
-            if (arg.type != TokenType::IMMEDIATE)
-                throw std::runtime_error("word directive expects immediate values as arguments");
-            std::vector<std::byte> immediateBytes = intStringToBytes(arg.value);
-            bytes.insert(bytes.end(), immediateBytes.begin(), immediateBytes.end());
-        }
-        return bytes;
-    }
 
-    if (dirName == "space") {
+        const std::string escapedString = escapeString(args[0].value);
+        bytes = parseAllocBlock(loc, escapedString.length(), 1);
+        populateMemBlock(bytes, escapedString, dirName == "asciiz");
+    }
+    // Insert a word
+    else if (dirName == "word") {
+        for (const Token& arg : args) {
+            if (arg.type != TokenType::IMMEDIATE || !isSignedInteger(arg.value))
+                throw std::runtime_error(dirName + " directive expects integers as arguments");
+
+            std::vector<std::byte> wordBytes = parseAllocBlock(loc, 4, 4);
+            populateMemBlock(wordBytes, std::stoi(arg.value));
+            bytes.insert(bytes.end(), wordBytes.begin(), wordBytes.end());
+        }
+    }
+    // Insert a space of the specified length
+    else if (dirName == "space") {
         if (args.size() != 1)
             throw std::runtime_error("space directive expects exactly one argument");
         if (args[0].type != TokenType::IMMEDIATE || !isSignedInteger(args[0].value) ||
             std::stoi(args[0].value) <= 0)
             throw std::runtime_error(dirName + " directive expects a positive integer argument");
 
-        for (int i = 0; i < std::stoi(args[0].value); ++i)
-            bytes.push_back(static_cast<std::byte>(0));
-        return bytes;
+        bytes = parseAllocBlock(loc, std::stoi(args[0].value), 1);
     }
+    // Insert padding to align next allocation
+    else if (dirName == "align") {
+        if (args.size() != 1)
+            throw std::runtime_error("align directive expects exactly one argument");
+        if (args[0].type != TokenType::IMMEDIATE || !isSignedInteger(args[0].value))
+            throw std::runtime_error(dirName + " directive expects a positive integer argument");
 
-    throw std::runtime_error("Unsupported directive " + dirName);
+        const int alignSize = std::stoi(args[0].value);
+        if (alignSize < 0 || alignSize > 3)
+            throw std::runtime_error("Alignment size must be between 0 and 3");
+        bytes = parseAllocBlock(loc, 0, 2 << alignSize);
+    } else
+        throw std::runtime_error("Unsupported directive " + dirName);
+
+    return bytes;
 }
 
 
 std::vector<std::byte> parseAllocBlock(const uint32_t loc, const int blockSize,
                                        const int blockAlign) {
+    if (blockSize < 0)
+        throw std::runtime_error("Block size must be positive");
+
     const uint32_t padding = blockAlign - (loc % blockAlign); // Pad to nearest multiple
     std::vector bytes(padding + blockSize, std::byte{0});
     return bytes;
