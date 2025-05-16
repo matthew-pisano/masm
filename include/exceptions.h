4bf9b89a6df657558332d93dedd3188cda5b6a1c
matthew-pisano <matthewpisano14@gmail.com>
2025-05-15 20:53:45 -0400

    Add Exceptions Header

---
+++ include/exceptions.h
@@ -0,0 +1,40 @@
+//
+// Created by matthew on 5/15/25.
+//
+
+#ifndef EXCEPTIONS_H
+#define EXCEPTIONS_H
+
+#include <stdexcept>
+
+
+/**
+ * A base class for all MASM exceptions
+ */
+class MasmException : public std::runtime_error {
+public:
+    explicit MasmException(const std::string& message) : std::runtime_error(message) {}
+    [[nodiscard]] const char* what() const noexcept override { return std::runtime_error::what(); }
+};
+
+
+/**
+ * A class for syntax errors in MASM
+ */
+class MasmSyntaxError final : public MasmException {
+public:
+    explicit MasmSyntaxError(const std::string& message) : MasmException(message) {}
+    [[nodiscard]] const char* what() const noexcept override { return MasmException::what(); }
+};
+
+
+/**
+ * A class for runtime errors in MASM
+ */
+class MasmRuntimeError final : public MasmException {
+public:
+    explicit MasmRuntimeError(const std::string& message) : MasmException(message) {}
+    [[nodiscard]] const char* what() const noexcept override { return MasmException::what(); }
+};
+
+#endif // EXCEPTIONS_H
