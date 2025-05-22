137cd34390b378b5c8e73c6efcdaaf26c22f8336
matthew-pisano <matthewpisano14@gmail.com>
2025-05-22 01:20:20 -0400

    Add Tests for Add Instructions

--- test/test_instructions.cpp
+++ test/test_instructions.cpp
@@ -15,8 +15,7 @@
 TEST_CASE("Test add Instruction") {
     const RawFile rawFile = makeRawFile({"add $t0, $t1, $t2"});
     const std::vector<SourceLine> actualTokens = Tokenizer::tokenizeFile({rawFile});
-
-    SECTION("Test Tokenize add") {
+    SECTION("Test Tokenize") {
         const std::vector<std::vector<Token>> expectedTokens = {{{TokenType::INSTRUCTION, "add"},
                                                                  {TokenType::REGISTER, "t0"},
                                                                  {TokenType::SEPERATOR, ","},
@@ -28,8 +27,7 @@ TEST_CASE("Test add Instruction") {
 
     Parser parser{};
     const MemLayout actualLayout = parser.parse(actualTokens);
-
-    SECTION("Test Parse add") {
+    SECTION("Test Parse") {
         const std::vector<std::byte> expectedBytes = intVec2ByteVec({0x01, 0x2a, 0x40, 0x20});
         const std::vector<std::byte> actualBytes = actualLayout.at(MemSection::TEXT);
         REQUIRE(expectedBytes == actualBytes);
@@ -37,11 +35,111 @@ TEST_CASE("Test add Instruction") {
 
     Interpreter interpreter{std::cin, std::cout};
     interpreter.setUpdateMMIO(false);
-    interpreter.getState().registers[Register::T1] = 1;
+    interpreter.getState().registers[Register::T1] = -1;
     interpreter.getState().registers[Register::T2] = 2;
     interpreter.interpret(actualLayout);
-    SECTION("Test Execute add") {
-        constexpr int32_t expectedResult = 3;
+    SECTION("Test Execute") {
+        constexpr int32_t expectedResult = 1;
+        const int32_t actualResult = interpreter.getState().registers[Register::T0];
+        REQUIRE(expectedResult == actualResult);
+    }
+}
+
+
+TEST_CASE("Test addu Instruction") {
+    const RawFile rawFile = makeRawFile({"addu $t0, $t1, $t2"});
+    const std::vector<SourceLine> actualTokens = Tokenizer::tokenizeFile({rawFile});
+    SECTION("Test Tokenize") {
+        const std::vector<std::vector<Token>> expectedTokens = {{{TokenType::INSTRUCTION, "addu"},
+                                                                 {TokenType::REGISTER, "t0"},
+                                                                 {TokenType::SEPERATOR, ","},
+                                                                 {TokenType::REGISTER, "t1"},
+                                                                 {TokenType::SEPERATOR, ","},
+                                                                 {TokenType::REGISTER, "t2"}}};
+        REQUIRE_NOTHROW(validateTokenLines(expectedTokens, actualTokens));
+    }
+
+    Parser parser{};
+    const MemLayout actualLayout = parser.parse(actualTokens);
+    SECTION("Test Parse") {
+        const std::vector<std::byte> expectedBytes = intVec2ByteVec({0x01, 0x2a, 0x40, 0x21});
+        const std::vector<std::byte> actualBytes = actualLayout.at(MemSection::TEXT);
+        REQUIRE(expectedBytes == actualBytes);
+    }
+
+    Interpreter interpreter{std::cin, std::cout};
+    interpreter.setUpdateMMIO(false);
+    interpreter.getState().registers[Register::T1] = -1;
+    interpreter.getState().registers[Register::T2] = 2;
+    interpreter.interpret(actualLayout);
+    SECTION("Test Execute") {
+        constexpr int32_t expectedResult = 1;
+        const int32_t actualResult = interpreter.getState().registers[Register::T0];
+        REQUIRE(expectedResult == actualResult);
+    }
+}
+
+
+TEST_CASE("Test addi Instruction") {
+    const RawFile rawFile = makeRawFile({"addi $t0, $t1, -2"});
+    const std::vector<SourceLine> actualTokens = Tokenizer::tokenizeFile({rawFile});
+    SECTION("Test Tokenize") {
+        const std::vector<std::vector<Token>> expectedTokens = {{{TokenType::INSTRUCTION, "addi"},
+                                                                 {TokenType::REGISTER, "t0"},
+                                                                 {TokenType::SEPERATOR, ","},
+                                                                 {TokenType::REGISTER, "t1"},
+                                                                 {TokenType::SEPERATOR, ","},
+                                                                 {TokenType::IMMEDIATE, "-2"}}};
+        REQUIRE_NOTHROW(validateTokenLines(expectedTokens, actualTokens));
+    }
+
+    Parser parser{};
+    const MemLayout actualLayout = parser.parse(actualTokens);
+    SECTION("Test Parse") {
+        const std::vector<std::byte> expectedBytes = intVec2ByteVec({0x21, 0x28, 0xff, 0xfe});
+        const std::vector<std::byte> actualBytes = actualLayout.at(MemSection::TEXT);
+        REQUIRE(expectedBytes == actualBytes);
+    }
+
+    Interpreter interpreter{std::cin, std::cout};
+    interpreter.setUpdateMMIO(false);
+    interpreter.getState().registers[Register::T1] = -1;
+    interpreter.interpret(actualLayout);
+    SECTION("Test Execute") {
+        constexpr int32_t expectedResult = -3;
+        const int32_t actualResult = interpreter.getState().registers[Register::T0];
+        REQUIRE(expectedResult == actualResult);
+    }
+}
+
+
+TEST_CASE("Test addiu Instruction") {
+    const RawFile rawFile = makeRawFile({"addiu $t0, $t1, -2"});
+    const std::vector<SourceLine> actualTokens = Tokenizer::tokenizeFile({rawFile});
+    SECTION("Test Tokenize") {
+        const std::vector<std::vector<Token>> expectedTokens = {{{TokenType::INSTRUCTION, "addiu"},
+                                                                 {TokenType::REGISTER, "t0"},
+                                                                 {TokenType::SEPERATOR, ","},
+                                                                 {TokenType::REGISTER, "t1"},
+                                                                 {TokenType::SEPERATOR, ","},
+                                                                 {TokenType::IMMEDIATE, "-2"}}};
+        REQUIRE_NOTHROW(validateTokenLines(expectedTokens, actualTokens));
+    }
+
+    Parser parser{};
+    const MemLayout actualLayout = parser.parse(actualTokens);
+    SECTION("Test Parse") {
+        const std::vector<std::byte> expectedBytes = intVec2ByteVec({0x25, 0x28, 0xff, 0xfe});
+        const std::vector<std::byte> actualBytes = actualLayout.at(MemSection::TEXT);
+        REQUIRE(expectedBytes == actualBytes);
+    }
+
+    Interpreter interpreter{std::cin, std::cout};
+    interpreter.setUpdateMMIO(false);
+    interpreter.getState().registers[Register::T1] = -1;
+    interpreter.interpret(actualLayout);
+    SECTION("Test Execute") {
+        constexpr int32_t expectedResult = -3;
         const int32_t actualResult = interpreter.getState().registers[Register::T0];
         REQUIRE(expectedResult == actualResult);
     }
