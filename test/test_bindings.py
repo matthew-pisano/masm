abaf184de8678cea581a8180e95c2dedd2148dcd
matthew-pisano <matthewpisano14@gmail.com>
2025-05-26 16:45:10 -0400

    Test Syscall Input and Output with PyBind

--- test/test_bindings.py
+++ test/test_bindings.py
@@ -5,26 +5,30 @@ import pytest
 
 
 @pytest.fixture(scope="module")
-def syscall_asm():
-    with open("test/fixtures/syscall/syscall.asm") as f:
+def syscall_io_asm():
+    with open("test/fixtures/input_output/input_output.asm") as f:
         return f.read()
 
 
 class TestBindings:
 
-    def test_syscall_io(self, syscall_asm: str):
-        raw_lines = syscall_asm.split("\n")
-        raw_file = pymasm.tokenizer.RawFile("syscall.asm", raw_lines)
+    def test_syscall_io(self, syscall_io_asm: str):
+        raw_lines = syscall_io_asm.split("\n")
+        raw_file = pymasm.tokenizer.RawFile("input_output.asm", raw_lines)
         program = pymasm.tokenizer.Tokenizer.tokenize([raw_file])
 
         parser = pymasm.parser.Parser()
         layout = parser.parse(program)
 
         io_mode = pymasm.interpreter.IOMode.SYSCALL
-        istream = BytesIO()
+        input_string = "5\n"
+        istream = BytesIO(input_string.encode())
         ostream = BytesIO()
         interpreter = pymasm.interpreter.Interpreter(io_mode, istream, ostream)
         exitCode = interpreter.interpret(layout)
 
+        with open("test/fixtures/input_output/input_output.txt", "rb") as f:
+            expected_output = f.read()
+
         assert exitCode == 0
-        assert ostream.getvalue() == b"hello"
+        assert ostream.getvalue() == expected_output
