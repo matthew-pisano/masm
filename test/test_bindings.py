b55e97a9ab32cfdde61baac1a8f8e2dcd6d672f5
matthew-pisano <matthewpisano14@gmail.com>
2025-05-26 20:23:58 -0400

    Add Python Test for MMIO

--- test/test_bindings.py
+++ test/test_bindings.py
@@ -10,6 +10,12 @@ def syscall_io_asm():
         return f.read()
 
 
+@pytest.fixture(scope="module")
+def mem_mapped_io_asm():
+    with open("test/fixtures/mmio/mmio.asm") as f:
+        return f.read()
+
+
 class TestBindings:
 
     def test_syscall_io(self, syscall_io_asm: str):
@@ -28,6 +34,27 @@ class TestBindings:
         exitCode = interpreter.interpret(layout)
 
         with open("test/fixtures/input_output/input_output.txt", "rb") as f:
+            expected_output = f.read()
+
+        assert exitCode == 0
+        assert ostream.getvalue() == expected_output
+
+    def test_mem_mapped_io(self, mem_mapped_io_asm: str):
+        raw_lines = mem_mapped_io_asm.split("\n")
+        raw_file = pymasm.tokenizer.RawFile("mmio.asm", raw_lines)
+        program = pymasm.tokenizer.Tokenizer.tokenize([raw_file])
+
+        parser = pymasm.parser.Parser()
+        layout = parser.parse(program)
+
+        io_mode = pymasm.interpreter.IOMode.MMIO
+        input_string = "1234"
+        istream = BytesIO(input_string.encode())
+        ostream = BytesIO()
+        interpreter = pymasm.interpreter.Interpreter(io_mode, istream, ostream)
+        exitCode = interpreter.interpret(layout)
+
+        with open("test/fixtures/mmio/mmio.txt", "rb") as f:
             expected_output = f.read()
 
         assert exitCode == 0
