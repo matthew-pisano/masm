6e7032580240b3572608b50a3674f796361063b5
matthew-pisano <matthewpisano14@gmail.com>
2025-06-06 17:35:09 -0400

    Add FP Add Instruction Tests

--- test/test_instructions/test_cp1_instructions.cpp
+++ test/test_instructions/test_cp1_instructions.cpp
@@ -104,11 +104,75 @@ TEST_CASE("Test FP Abs.d Instruction") {
         REQUIRE(expectedBytes == actualBytes);
     }
 
-    constexpr float64_t expected = 42.69;
+    constexpr float64_t expected = 42e69;
     DebugInterpreter interpreter{IOMode::SYSCALL, std::cin, std::cout};
     interpreter.getState().cp1.setDouble(Coproc1Register::F2, expected * -1);
     interpreter.interpret(actualLayout);
     SECTION("Test Execute") {
+        REQUIRE(expected == interpreter.getState().cp1.getDouble(Coproc1Register::F0));
+    }
+}
+
+
+TEST_CASE("Test FP Add.s Instruction") {
+    const SourceFile rawFile = makeRawFile({"add.s $f0, $f1, $f2"});
+    const std::vector<LineTokens> actualTokens = Tokenizer::tokenizeFile({rawFile});
+    SECTION("Test Tokenize") {
+        const std::vector<std::vector<Token>> expectedTokens = {{{TokenType::INSTRUCTION, "add.s"},
+                                                                 {TokenType::REGISTER, "f0"},
+                                                                 {TokenType::SEPERATOR, ","},
+                                                                 {TokenType::REGISTER, "f1"},
+                                                                 {TokenType::SEPERATOR, ","},
+                                                                 {TokenType::REGISTER, "f2"}}};
+        REQUIRE_NOTHROW(validateTokenLines(expectedTokens, actualTokens));
+    }
+
+    DebugParser parser{};
+    const MemLayout actualLayout = parser.parse(actualTokens);
+    SECTION("Test Parse") {
+        const std::vector<std::byte> expectedBytes = intVec2ByteVec({0x46, 0x02, 0x08, 0x00});
+        const std::vector<std::byte> actualBytes = actualLayout.data.at(MemSection::TEXT);
+        REQUIRE(expectedBytes == actualBytes);
+    }
+
+    DebugInterpreter interpreter{IOMode::SYSCALL, std::cin, std::cout};
+    interpreter.getState().cp1.setFloat(Coproc1Register::F1, 10);
+    interpreter.getState().cp1.setFloat(Coproc1Register::F2, 20);
+    interpreter.interpret(actualLayout);
+    SECTION("Test Execute") {
+        constexpr float32_t expected = 30;
+        REQUIRE(expected == interpreter.getState().cp1.getFloat(Coproc1Register::F0));
+    }
+}
+
+
+TEST_CASE("Test FP Add.d Instruction") {
+    const SourceFile rawFile = makeRawFile({"add.d $f0, $f2, $f4"});
+    const std::vector<LineTokens> actualTokens = Tokenizer::tokenizeFile({rawFile});
+    SECTION("Test Tokenize") {
+        const std::vector<std::vector<Token>> expectedTokens = {{{TokenType::INSTRUCTION, "add.d"},
+                                                                 {TokenType::REGISTER, "f0"},
+                                                                 {TokenType::SEPERATOR, ","},
+                                                                 {TokenType::REGISTER, "f2"},
+                                                                 {TokenType::SEPERATOR, ","},
+                                                                 {TokenType::REGISTER, "f4"}}};
+        REQUIRE_NOTHROW(validateTokenLines(expectedTokens, actualTokens));
+    }
+
+    DebugParser parser{};
+    const MemLayout actualLayout = parser.parse(actualTokens);
+    SECTION("Test Parse") {
+        const std::vector<std::byte> expectedBytes = intVec2ByteVec({0x46, 0x24, 0x10, 0x00});
+        const std::vector<std::byte> actualBytes = actualLayout.data.at(MemSection::TEXT);
+        REQUIRE(expectedBytes == actualBytes);
+    }
+
+    DebugInterpreter interpreter{IOMode::SYSCALL, std::cin, std::cout};
+    interpreter.getState().cp1.setDouble(Coproc1Register::F2, 10);
+    interpreter.getState().cp1.setDouble(Coproc1Register::F4, 20);
+    interpreter.interpret(actualLayout);
+    SECTION("Test Execute") {
+        constexpr float32_t expected = 30;
         REQUIRE(expected == interpreter.getState().cp1.getDouble(Coproc1Register::F0));
     }
 }
