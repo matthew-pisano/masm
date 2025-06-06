35fb83b46626e39ab596ed9bb714944b217712fd
matthew-pisano <matthewpisano14@gmail.com>
2025-06-06 17:06:48 -0400

    Add FP Instruction Tests

---
+++ test/test_instructions/test_cp1_instructions.cpp
@@ -0,0 +1,40 @@
+//
+// Created by matthew on 6/6/25.
+//
+
+
+#include <catch2/catch_test_macros.hpp>
+
+#include "../testing_utilities.h"
+#include "interpreter/interpreter.h"
+#include "interpreter/memory.h"
+#include "tokenizer/tokenizer.h"
+
+
+TEST_CASE("Test FP Abs.s Instruction") {
+    const SourceFile rawFile = makeRawFile({"abs.s $f0, $f1"});
+    const std::vector<LineTokens> actualTokens = Tokenizer::tokenizeFile({rawFile});
+    SECTION("Test Tokenize") {
+        const std::vector<std::vector<Token>> expectedTokens = {{{TokenType::INSTRUCTION, "abs.s"},
+                                                                 {TokenType::REGISTER, "f0"},
+                                                                 {TokenType::SEPERATOR, ","},
+                                                                 {TokenType::REGISTER, "f1"}}};
+        REQUIRE_NOTHROW(validateTokenLines(expectedTokens, actualTokens));
+    }
+
+    DebugParser parser{};
+    const MemLayout actualLayout = parser.parse(actualTokens);
+    SECTION("Test Parse") {
+        const std::vector<std::byte> expectedBytes = intVec2ByteVec({0x46, 0x00, 0x08, 0x05});
+        const std::vector<std::byte> actualBytes = actualLayout.data.at(MemSection::TEXT);
+        REQUIRE(expectedBytes == actualBytes);
+    }
+
+    constexpr float32_t expected = 42.69;
+    DebugInterpreter interpreter{IOMode::SYSCALL, std::cin, std::cout};
+    interpreter.getState().cp1.setFloat(Coproc1Register::F1, expected * -1);
+    interpreter.interpret(actualLayout);
+    SECTION("Test Execute") {
+        REQUIRE(expected == interpreter.getState().cp1.getFloat(Coproc1Register::F0));
+    }
+}
