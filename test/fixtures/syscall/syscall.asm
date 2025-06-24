f0c46d24dd181ae3ca928bcfe41918b3d239f3de
matthew-pisano <matthewpisano14@gmail.com>
2025-06-24 08:03:51 -0400

    Update Tests for Syscalls

--- test/fixtures/syscall/syscall.asm
+++ test/fixtures/syscall/syscall.asm
@@ -1,12 +1,31 @@
 .data
 
-string: .asciiz "hello"
+string: .asciiz "hello\n"
 
 .text
 
 main:
+    # Print String
 	li $v0, 4
 	la $a0, string
+	syscall
+
+	# Set rand seed
+	li $v0, 40
+	li $a0, 0
+	li $a1, 42
+	syscall
+
+	# Get random float
+	li $v0, 43
+	li $a0, 0
+	syscall
+
+	# Move from $f0 to $f12
+	mov.s $f12, $f0
+
+	# Print float
+	li $v0, 2
 	syscall
 
 	li $v0, 10
