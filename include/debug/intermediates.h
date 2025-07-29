8dc5a133e0d60777490e80e6f2da791904561d34
matthew-pisano <matthewpisano14@gmail.com>
2025-07-28 21:11:27 -0400

    Add Preprocessed Intermediate Generator

---
+++ include/debug/intermediates.h
@@ -0,0 +1,30 @@
+//
+// Created by matthew on 7/28/25.
+//
+
+#ifndef INTERMEDIATES_H
+#define INTERMEDIATES_H
+#include <string>
+
+#include "interpreter/memory.h"
+#include "parser/labels.h"
+
+
+/**
+ * Converts a memory layout to a human-readable string representation (preprocessed assembly)
+ * @param layout The memory layout to convert
+ * @param labelMap The label map to use for resolving labels
+ * @return A string representation of the memory layout
+ */
+std::string layoutAsString(MemLayout& layout, const LabelMap& labelMap);
+
+
+/**
+ * Converts a memory layout to a binary representation, which is a vector of bytes
+ * @param layout The memory layout to convert
+ * @return A vector of bytes representing the memory layout in binary form
+ */
+std::vector<std::byte> layoutAsBinary(MemLayout& layout);
+
+
+#endif // INTERMEDIATES_H
---
+++ src/debug/intermediates.cpp
@@ -0,0 +1,56 @@
+//
+// Created by matthew on 7/28/25.
+//
+
+#include "debug/intermediates.h"
+
+
+std::string memSectionToName(const MemSection& section) {
+    switch (section) {
+        case MemSection::DATA:
+            return "data";
+        case MemSection::HEAP:
+            return "heap";
+        case MemSection::GLOBAL:
+            return "global";
+        case MemSection::STACK:
+            return "stack";
+        case MemSection::TEXT:
+            return "text";
+        case MemSection::KTEXT:
+            return "ktext";
+        case MemSection::KDATA:
+            return "kdata";
+        case MemSection::MMIO:
+            return "mmio";
+    }
+    throw std::runtime_error("Unknown memory section");
+}
+
+
+std::string layoutAsString(const MemLayout& layout, const LabelMap& labelMap) {
+    std::string program;
+
+    for (const auto& [section, data] : layout.data) {
+        const uint32_t sectionOffset = memSectionOffset(section);
+        program += "." + memSectionToName(section) + "\n";
+
+        for (uint32_t i = 0; i < data.size(); i += 4) {
+            uint32_t address = sectionOffset + i;
+            DebugInfo debugInfo = layout.debugInfo.at(address);
+
+            // Add label if it exists
+            try {
+                program += labelMap.lookupLabel(address) + ":\n";
+            } catch (const std::runtime_error&) {
+            }
+
+            // Add instruction or data
+            program += debugInfo.source.text + "\n";
+        }
+    }
+
+    return program;
+}
+
+std::vector<std::byte> layoutAsBinary(MemLayout& layout) {}
