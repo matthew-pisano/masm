2aa1db3fa90900687cc68e3c4fe3fc601e05f4f9
matthew-pisano <matthewpisano14@gmail.com>
2026-05-21 00:33:11 -0400

    Update Wildcard Tests

--- tests/utils/test_fileio.cpp
+++ tests/utils/test_fileio.cpp
@@ -17,6 +17,13 @@ TEST_CASE("Test Wild Card Resolution") {
         test = "hello/there.kenobi";
         REQUIRE(test == resolveWildcards({test})[0]);
     }
+
+    SECTION("Test Tilde Resolution") {
+        std::string inputPath = "~/hello/there";
+        std::vector<std::string> expectedPaths = {std::string(getenv("HOME")) + "/hello/there"};
+        REQUIRE(expectedPaths == resolveWildcards({inputPath}));
+    }
+
     SECTION("Test Single Wildcard") {
         std::vector<std::string> inputPaths = {"tests/fixtures/arithmetic/*.asm"};
         std::vector<std::string> expectedPaths = {"tests/fixtures/arithmetic/arithmetic.asm"};
@@ -27,10 +34,31 @@ TEST_CASE("Test Wild Card Resolution") {
                          "tests/fixtures/arithmetic/arithmetic.tkn", "tests/fixtures/arithmetic/arithmetic.txt"};
         REQUIRE(expectedPaths == resolveWildcards(inputPaths));
     }
+
     SECTION("Test Multiple Wildcard") {
-        std::vector<std::string> inputPaths = {"tests/fixtures/arithmetic/*.t*"};
+        std::vector<std::string> inputPaths = {"tests/fixtures/arithmetic/*.t*", "tests/fixtures/arithmetic/*.a*"};
+        std::vector<std::string> expectedPaths = {"tests/fixtures/arithmetic/arithmetic.tkn",
+                                                  "tests/fixtures/arithmetic/arithmetic.txt",
+                                                  "tests/fixtures/arithmetic/arithmetic.asm"};
+        REQUIRE(expectedPaths == resolveWildcards(inputPaths));
+    }
+
+    SECTION("Test Question Mark Wildcard") {
+        std::vector<std::string> inputPaths = {"tests/fixtures/arithmetic/arithmeti?.txt"};
+        std::vector<std::string> expectedPaths = {"tests/fixtures/arithmetic/arithmetic.txt"};
+        REQUIRE(expectedPaths == resolveWildcards(inputPaths));
+    }
+
+    SECTION("Test Mixed Wildcard") {
+        std::vector<std::string> inputPaths = {"tests/fixtures/arithmetic/arithmeti?.t*"};
         std::vector<std::string> expectedPaths = {"tests/fixtures/arithmetic/arithmetic.tkn",
                                                   "tests/fixtures/arithmetic/arithmetic.txt"};
+        REQUIRE(expectedPaths == resolveWildcards(inputPaths));
+    }
+
+    SECTION("Test No Matches") {
+        std::vector<std::string> inputPaths = {"tests/fixtures/arithmetic/arithmetic.g*"};
+        std::vector<std::string> expectedPaths = {"tests/fixtures/arithmetic/arithmetic.g*"};
         REQUIRE(expectedPaths == resolveWildcards(inputPaths));
     }
 }
