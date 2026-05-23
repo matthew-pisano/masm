1f013c62984264a75169d0af3cf9366e90446ba7
matthew-pisano <matthewpisano14@gmail.com>
2026-05-23 00:27:02 -0400

    Fix Versioning in Wheel

--- python/pymasm/__init__.py
+++ python/pymasm/__init__.py
@@ -1,9 +1,11 @@
 """MIPS Assembly Simulator Library"""
 
 from pymasm.pymasm_core import exceptions
-from pymasm.pymasm_core import simulator
 from pymasm.pymasm_core import parser
+from pymasm.pymasm_core import simulator
 from pymasm.pymasm_core import tokenizer
+
+from ._version import __version__
 
 
 __all__ = ["tokenizer", "parser", "simulator", "exceptions"]
